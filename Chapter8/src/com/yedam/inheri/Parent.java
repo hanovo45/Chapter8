4ecec41bfec55a1fde2d8c9258f719153d065c17
admin <admin@YD05-12>
2023-02-03 17:59:09 +0900

    Chapter8

---
+++ Chapter8/src/com/yedam/inheri/Parent.java
@@ -0,0 +1,12 @@
+package com.yedam.inheri;
+
+public class Parent {
+	//필드
+	protected String firstName;
+	
+	//생성자
+	
+	public void getInfo() {
+		System.out.println("우리 가족의 성씨는"+firstName+"입니다.");
+	}
+}
